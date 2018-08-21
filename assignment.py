50d0b32e1f2d42db50534c41ebcf58da82bc675e
JamesElgey <james.elgey@my.jcu.edu.au>
2018-08-21 17:24:53 +1000

    Update 1

--- assignment.py
+++ assignment.py
@@ -1,14 +1,25 @@
 def main():
+    str_count = 0
+    book_dict = {}
     in_file = open("book_list.csv", "r")
-    first_line = in_file.readline()
-    print(first_line)
     print("Welcome to Reading Tracker 1.0 by James Elgey")
     #TODO load up csv file
-    print("This is the menu: \nOption 1 \nOption 2 \nOption 3")
+    print("Menu: \nL - List all books \nA - Add a new book \nM - Mark a book as completed \nQ - Quit")
     user_choice = get_user_choice()
     while user_choice != 'q':
         if user_choice == 'l':
-            print("List all books")
+            number = 1
+            for line in in_file:
+                word_str = 0
+                book_information = line.split(",")
+                book_dict[number] = book_information
+                for char in book_information[0]:
+                    word_str = word_str + 1
+                    if word_str > str_count:
+                        str_count = word_str
+                number = number + 1
+            for item in book_dict:
+                print(item)
         elif user_choice == 'a':
             print("Add new book")
         elif user_choice == 'm':
